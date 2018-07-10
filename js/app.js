0db0f00535ee4e77e9db4b80fbea23c0f86d7cf1
ninakwelch <ninakwelch@gmail.com>
2018-07-10 20:03:28 +0100

    Ajax requests added to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
@@ -10,3 +11,20 @@
         searchedForText = searchField.value;
     });
 })();
+
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID c2527a62971ea8d537f44eac3b6397bf9e88ea4ada8400131d3bcfd22481c0c7');
+unsplashRequest.send();
+
+function addImage(){
+}
+
+function addArticles () {}
+const articleRequest = new XMLHttpRequest();
+articleRequest.onload = addArticles;
+articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=68bec75cdab74728b85e1e916a9ba0c5`);
+articleRequest.send();
